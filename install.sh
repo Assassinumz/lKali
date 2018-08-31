196dc0e48272932a92877d4aac91e7e4745e9083
umz <kumraz858@gmail.com>
2018-08-31 19:06:17 +0530

    install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 clear
 echo "=======Installing lKali======="
-sudo cp lKali /usr/bin/lKali
+sudo cp lKali.py /usr/bin/lKali
 sudo chmod +x /usr/bin/lKali
 echo "Open terminal and type lkali to start the script"
